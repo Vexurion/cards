b63e1da6f528dc35725a16f67ecd8214f656d6e4
Alexx <Vexurion@gmail.com>
2020-02-27 10:10:20 -0700

    Implemented a comparator lambda.

--- src/edu/cnm/deepdive/model/Deck.java
+++ src/edu/cnm/deepdive/model/Deck.java
@@ -48,9 +48,13 @@ public class Deck {
    * @param rng Randomizes the cards
    */
   public void shuffle(Random rng) {
+    gather();
+    Collections.shuffle(cards, rng);
+  }
+
+  private void gather() {
     cards.addAll(dealt);
     dealt.clear();
-    Collections.shuffle(cards, rng);
   }
 
   /**
@@ -72,4 +76,18 @@ public class Deck {
       return cards.toString();
   }
 
+  public void sort(boolean gather) {
+   if (gather) {
+     gather();
+   }
+   cards.sort((card1, card2) -> {
+     int result = card1.getSuit().compareTo(card2.getSuit());
+     if (result == 0) {
+       result = card1.getRank().compareTo(card2.getRank());
+     }
+     return result;
+   });
+  }
+  //time complexity
+  //space complexity of merge sort
 }
